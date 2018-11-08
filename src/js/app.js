8822454a34e233b2cccab9a3d09491d05843e6ca
marimeli <marimel.casas@gmail.com>
2018-11-08 06:15:44 -0500

    uniendo archivos

---
+++ src/js/app.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instance = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true,
+  });
+  
+  // instance.next(3)
+  autoplay()
+  function autoplay() {
+    const element = document.querySelector('.carousel');
+    const instance2 = M.Carousel.getInstance(element);
+    instance2.next();
+    setTimeout(autoplay, 3000);
+  }
+});
+
+const btn = document.getElementsByTagName('button');
+const section = document.getElementsByTagName('section');
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', () => {
+    for (let x = 0; x < section.length; x++) {
+      if (i === x) {
+        section[x].style.display = 'block'
+      } else if (i > x) {
+        let z = i - 5;
+        if (z == x) {
+          console.log(x)
+          section[x].style.display = 'block'
+        }
+        else {
+          section[x].style.display = 'none'
+        }
+      } else {
+        section[x].style.display = 'none'
+      }
+    }
+
+  }
+  )
+}
+
+
+
+
+
--- src/js/dom.js
+++ src/js/dom.js
@@ -1,3 +1,5 @@
+const auth = firebase.auth();
+
 //Login inputs
 const mail = document.getElementById('email');
 const password = document.getElementById('password');
@@ -19,45 +21,22 @@ const logoutButton = document.getElementById('logout');
 //Secciones
 const signInSection = document.getElementById('sign-in'); 
 const loginSection = document.getElementById('login')
-const productsSection = document.getElementById('products-cards');
-
-//Contenido de tablero
-const tableContent = document.getElementById('table-content');
-
-
-//Cuando inicia sesión, esta función muestra nueva seccion y oculta la anterior.
-const showProductsSection = () => {
-    productsSection.style.display = 'block';
-    loginSection.style.display = 'none';
-    signInSection.style.display = 'none';  
-};
 
 //Cuando cierra sesión, esta función muestra la sección de login.
 const showLogin = () => {
-    productsSection.style.display = 'none';
     loginSection.style.display = 'block';
-    signInSection.style.display = 'none';  
-};
-
-//Cuando da clic en el botón para registar nueva cuenta, esta función muestra la sección de registro.
-const showRegister = () => {
-    loginSection.style.display = 'none';
-    signInSection.style.display = 'block';
-    productsSection.style.display = 'none';
-};
-
-//Cuando ingresa a la pestaña, esta función solo muestra la sección de login.
-const showOnlyLogin = () => {
-    productsSection.style.display = 'none';
     signInSection.style.display = 'none';
-    loginSection.style.display = 'block';
+ };
+
+ //Cuando da clic en el botón para registar nueva cuenta, esta función muestra la sección de registro.
+const showRegister = () => {
+   loginSection.style.display = 'none';
+   signInSection.style.display = 'block';
 };
 
 /* DOM EVENTS */
 signinButton.addEventListener('click', wrtiteDataFirebase);
 loginButton.addEventListener('click', loginWithFirebase);
-/* logoutButton.addEventListener('click', logoutWithFirebase); */
-
-registerButton.addEventListener('click', showRegister);
+logoutButton.addEventListener('click', logoutWithFirebase);
 backButton.addEventListener('click', showLogin);
-
+registerButton.addEventListener('click', showRegister);
