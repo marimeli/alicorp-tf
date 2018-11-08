5c3e77c8b391f87eced0ad74311dd3917728afa5
Geraldine13 <geral1312@hotmail.com>
2018-11-08 10:18:09 -0500

    Muestra data

--- src/js/app.js
+++ src/js/app.js
@@ -111,3 +111,69 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+window.printProducts = () => {
+  firebase.database().ref('categorias/')
+    .on('value', (categoriasRef) => {
+      const listCategorias = categoriasRef.val();
+      const showProducts = document.getElementById('container-products');
+      showProducts.innerHTML = '';
+      const arrayCat = Object.keys(listCategorias)      
+      arrayCat.forEach((id) => {
+        const productsTotal = Object.keys(listCategorias[id]);
+        productsTotal.forEach(elem => {
+        const itemProducts = listCategorias[id][elem];
+        showProducts.innerHTML += `
+      <div class="row" id=${id}>
+      <div class="col s12 l4">
+      <div class="card">
+        <div class="card-image">
+          <img src="${itemProducts.imagen}">
+        </div>
+        <div class="card-content">
+          <a class="modal-trigger" href="#modal1">
+            <span class="card-title center-align">${itemProducts.producto}</span>
+          </a>
+          <p class="center-align">${itemProducts.marca}</p>
+          <br>
+          <p class="center-align">Precio: S/. ${itemProducts.precio}</p>
+        </div>
+
+        <div class="card-action center-align">
+          <input class="left-align" type="number" min="0" max="${itemProducts.stock}" id="cant">
+          <a class="right-align" href="#">
+            <i class="material-icons black-text">shopping_cart</i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal Structure -->
+  <div id="modal1" class="modal">
+    <div class="modal-content center-align">
+      <h5>${itemProducts.producto}</h5>
+    </div>
+    <div class="row details">
+      <div class="col s12 l6 center-align">
+        <img src="${itemProducts.imagen}">
+      </div>
+      <div class="col s12 l6 center-align">
+        <p class="description">${itemProducts.descripcion}</p>
+        <div class="operation">
+          <input class="left-align" type="number" min="0" id="cantModal">
+          <a class="right-align" href="#">
+            <i class="material-icons black-text">shopping_cart</i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Regresar</a>
+    </div>
+  </div>
+      `          
+        })
+      })
+     })
+}
